adf8290fa0b88d211d441c9240c3d01f771b9888
Mohamed Jabal <jabalmohamed341@gmail.com>
2025-09-10 12:59:27 +0200

    Complete 02-day-plan

--- exercises/02-sequence/01-three-logs/script.js
+++ exercises/02-sequence/01-three-logs/script.js
@@ -1,6 +1,9 @@
 // Exercise 1: Three Logs
 // Write three console.log() statements that show the steps of making a sandwich
-
+console.log("Getting bread and ingredients");
+console.log("Adding filling to the sandwich");
+console.log("Sandwich is ready to eat!");
+//
 // TODO: Add your first console.log() statement here (starting the process)
 
 // TODO: Add your second console.log() statement here (middle step)
--- exercises/02-sequence/02-day-plan/script.js
+++ exercises/02-sequence/02-day-plan/script.js
@@ -1,8 +1,12 @@
 // Day Plan Exercise
 // Write 3 console.log statements to print your daily activities
-
+console.log("Waking up and getting ready for the day");
+console.log("Working on coding exercises and projects");
+console.log("Relaxing and spending time with family in the evening");
 // TODO: Write your first activity here
+console.log("Waking up and getting ready for the day"); 
 
 // TODO: Write your second activity here
-
+console.log("Working on coding exercises and projects");
 // TODO: Write your third activity here
+console.log("Relaxing and spending time with family in the evening");   
